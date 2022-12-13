2470e9a78b6be013c0d62809cf25dfb33fb2419c
Stephen Brown <sabrown2@student.fullsail.edu>
2022-12-12 20:26:24 -0500

    Updating api

--- api/messages/messages.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  serverUp: "Service is Up.",
-  notFound: "HTTP Status: 404 Not Found"
-};
--- api/services/spotify.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-require("dotenv").config();
-
-const spotifyService = async () => {
-  return await axios.get(`${process.env.spotifyAPI}`);
-};
-
-module.exports = spotifyService;
