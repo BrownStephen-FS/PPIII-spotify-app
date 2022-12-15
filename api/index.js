404e191a27080838c65988be3d6849fe450953b9
Stephen Brown <sabrown2@student.fullsail.edu>
2022-12-14 19:03:29 -0500

    Updated project

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const bodyParser = require('body-parser')
-const spotify = require('./middlewares/spotify')
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(cors())
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+const spotify = require("./middlewares/spotify");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
-app.use('/spotify/v1', spotify)
+app.use("/spotify/v1", spotify);
 
-app.listen(3001)
+app.listen(3001);
